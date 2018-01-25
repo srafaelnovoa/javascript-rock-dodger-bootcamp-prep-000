c40385d1befcfa9be573b7cf8eb476d99d0b268e
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-25 22:23:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if (checkCollision(rock)) {endGame()}
+     if (checkCollision(rock)  && top >=360) {endGame()}
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
