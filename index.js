a4c2e693f829635b7523c223b2103eb8ba0facef
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-25 22:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function createRock(x) {
   }
 
   // We should kick of the animation of the rock around here
-  const myInterval = setInterval(moveRock, 40)
+  const myInterval = setInterval(moveRock, 4)
   
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
