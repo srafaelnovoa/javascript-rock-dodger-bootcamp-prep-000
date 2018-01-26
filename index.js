d296412eaf99a9fa3fb41242e0974336cbadff6c
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-26 01:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,6 +141,11 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
 }
 
 function moveDodgerLeft() {
