cab681c3d972289d179910aa491a99863c3b7468
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-26 02:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function createRock(x) {
          window.requestAnimationFrame(step)
          }
       }
-      window.requestAnimationFrame(moveRock)
+      window.requestAnimationFrame(step)
          
       /**
       * But if the rock *has* reached the bottom of the GAME,
