c90b2bb5b719147adf0e819b83b3c2e55edd2a11
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-26 01:30:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,10 +141,11 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
+   DODGER.addEventListener('keydown', function(e) {
+  if (e.which === LEFT_ARROW) {
     moveDodgerLeft()
   }
+  
 })
 }
 
