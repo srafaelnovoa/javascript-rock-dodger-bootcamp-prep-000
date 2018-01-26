d587da3fde420d971096278b570e79688032d511
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-26 02:10:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  gameInterval = null;
+  this.gameInterval = null;
   for (i = ROCKS.length - 1; i >=0; i--){
     ROCKS[i].remove();
   }
